81b67f3bfe9bb60c83720aaec42b065a519373ab
Freddy Leal <freddy.alejandro.leal@hotmail.com>
2023-09-09 21:42:03 -0500

    Add ardruino examples

---
+++ Ardruino examples/digital output/Blink/Blink.ino
@@ -0,0 +1,14 @@
+
+const int LED_BUILTIN = 4;
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(50);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(2000);                       // wait for a second
+  printf("Hello World");
+}
